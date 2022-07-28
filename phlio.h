dbc81ec2a64601e945d1b7d854b288ed97b5a0fa
Yihyun Choi <yichoi@c6r6s6.42seoul.kr>
2022-07-28 16:33:22 +0900

    header list

--- phlio.h
+++ phlio.h
@@ -6,13 +6,19 @@
 /*   By: yichoi <yichoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 00:30:33 by yichoi            #+#    #+#             */
-/*   Updated: 2022/07/28 00:31:17 by yichoi           ###   ########.fr       */
+/*   Updated: 2022/07/28 16:25:36 by yichoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <stdio.h>
+# include <string.h>	//memset, 
+# include <stdio.h>		// printf
+# include <stdlib.h>	// malloc, free
+# include <unistd.h>	// writem, usleep
+# include <sys/time.h>	// gettimeofday
+# include <pthread.h>	// pthread_create, _detach, _join, _mutex_init, mutex_destroy, mutex_lock, mutex_unlock
+
 
 #endif
